8d04d627bfd3fa0a3de3b9c3bb1a1979fa992fe1
Ridhisha Dangol <ridhishadangol@pingidentity.com>
2026-02-26 09:23:42 +0000

    feat [UIP-7706] Add Onyx tokens in Astro
    ## Describe your changes
    
    ## Any testing tips
    
    ## Checklist before requesting a review
    - [ ] I have performed a self-review of my code
    - [ ] I have added tests for any new or fixed functionality to prevent regressions
    - [ ] I have added clarifying code comments to blocks which other devs may have questions on
    
    Closes UIP-7706

---
+++ build/index.d.ts
@@ -0,0 +1,5 @@
+import * as astroTokensDark from './astro/tokens-dark.json';
+import * as astroTokens from './astro/tokens.json';
+import './scss/onyx-dark-stylesheet.scss';
+import './scss/onyx-stylesheet.scss';
+export { astroTokensDark, astroTokens };
