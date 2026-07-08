985ddcc275983c3b468e576db83a36a742ae13af
Kiran Gupta <kgupta@pingidentity.com>
2026-07-08 14:06:29 +0000

    feat: [UIP-8297] TableBase Update to match spec
    ## Describe your changes
    
    ## Any testing tips
    
    ## Checklist before requesting a review
    - [ ] I have performed a self-review of my code
    - [ ] I have added tests for any new or fixed functionality to prevent regressions
    - [ ] I have added clarifying code comments to blocks which other devs may have questions on
    
    Closes UIP-8297

--- src/tokens-dark/color/base.js
+++ src/tokens-dark/color/base.js
@@ -1,24 +1,27 @@
 import baseColors from '../../tokens/color/base.js';
 
-const swapped = key => ({
-    ...baseColors.color[key],
-    "dark": baseColors.color[key].light,
-    "light": baseColors.color[key].dark,
+const swapped = (key) => ({
+  ...baseColors.color[key],
+  dark: baseColors.color[key].light,
+  light: baseColors.color[key].dark,
 });
 
 export default {
-    "color": {
-        ...baseColors.color,
-        "blue": swapped("blue"),
-        "indigo": swapped("indigo"),
-        "purple": swapped("purple"),
-        "pink": swapped("pink"),
-        "red": swapped("red"),
-        "orange": swapped("orange"),
-        "yellow": swapped("yellow"),
-        "green": swapped("green"),
-        "teal": swapped("teal"),
-        "cyan": swapped("cyan"),
-        "gray": swapped("gray"),
-    }
-}
+  color: {
+    ...baseColors.color,
+    blue: swapped('blue'),
+    indigo: swapped('indigo'),
+    purple: swapped('purple'),
+    pink: swapped('pink'),
+    red: swapped('red'),
+    orange: swapped('orange'),
+    yellow: swapped('yellow'),
+    green: swapped('green'),
+    teal: swapped('teal'),
+    cyan: swapped('cyan'),
+    gray: swapped('gray'),
+    border: {
+      value: '#39414B',
+    },
+  },
+};
